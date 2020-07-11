8303ec67ae8dfd5e41ae245b0bcf1c549f42c8c5
Louis <Louis@alltraders.com>
2020-07-11 15:01:48 +0930

    9. Integration and Feature Test

--- app/Http/Controllers/Api/v1/DateTimeDiffController.php
+++ app/Http/Controllers/Api/v1/DateTimeDiffController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api\v1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
+use App\Libraries\Classes\Validator\DateTimeValidator;
+use App\Libraries\Classes\Validator\FilterValidator;
+use App\Libraries\Classes\DateTimeDiff\DateTimeDiffTools;
 
 /**
  * @method DateTimeDiff(Request $request, $start, $end, $filter = null)
@@ -19,6 +23,91 @@ class DateTimeDiffController extends Controller
      */
     public function DateTimeDiff(Request $request, $start, $end, $filter = null)
     {
-        return [];
+        $result = [];
+        try
+        {
+            //validate parameters
+            $validator = new DateTimeValidator;
+            $ret = $validator->IsValid($start);
+
+            if ($ret === 1)
+            {
+                $result['code'] = 40011;  
+                $result['state'] = 'error';
+                $result['message'] = 'The start datetime is empty.';
+                
+                return $result;
+            }
+            else if ($ret === 2)
+            {
+                $result['code'] = 40012;  
+                $result['state'] = 'error';
+                $result['message'] = 'The start datetime format is not ISO 8601 format.';
+
+                return $result;
+            }
+
+            $validator = new DateTimeValidator;
+            $ret = $validator->IsValid($end);
+
+            if ($ret === 1)
+            {
+                $result['code'] = 40011;  
+                $result['state'] = 'error';
+                $result['message'] = 'The end datetime is empty.';
+
+                return $result;
+            }
+            else if ($ret === 2)
+            {
+                $result['code'] = 40012;  
+                $result['state'] = 'error';
+                $result['message'] = 'The end datetime format is not ISO 8601 format.';
+
+                return $result;
+            }
+
+            $validator = new FilterValidator;
+            $filter = $validator->Transfer($filter);
+            $ret = $validator->IsValid($filter);
+
+            if ($ret === 1)
+            {
+                $result['code'] = 40011;  
+                $result['state'] = 'error';
+                $result['message'] = 'The filter is empty.';
+
+                return $result;
+            }
+            else if ($ret === 2)
+            {
+                $result['code'] = 40012;  
+                $result['state'] = 'error';
+                $result['message'] = 'The filter format is wrong.';
+
+                return $result;
+            }
+
+            $datetimediff_tools = new DateTimeDiffTools($filter);
+            $ret = $datetimediff_tools->GetDateTimeDiff($start, $end);
+
+            $result['code'] = 200;  
+            $result['state'] = 'success';
+            $result['message'] = 'Date time diff query succeed.';
+            $result['data'] = $ret;
+
+            return $result;
+        }
+        catch (\Exception $exp)
+        {
+            header('HTTP/1.1 500 Internal Server Error');
+            $result['code'] = 500;  
+            $result['state'] = 'error';
+            $result['message'] = $exp->getMessage();
+
+            return $result;
+        }
+
+        return $result;
     }
 }
--- tests/Feature/DateTimeDiffTest.php
+++ tests/Feature/DateTimeDiffTest.php
@@ -2,7 +2,7 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\Refreshresponsebase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
@@ -15,8 +15,40 @@ class DateTimeDiffTest extends TestCase
      */
     public function testExample()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('http://127.0.0.1:8000/api/v1/datetimediff/2020-07-22/2020-07-13/');
         $response->assertStatus(200);
+        $this->assertEquals($response->getData()->code, 200);
+        $this->assertEquals($response->getData()->state, 'success');
+        $this->assertEquals($response->getData()->message, 'Date time diff query succeed.');
+        $this->assertEquals($response->getData()->data->days, 9);
+        $this->assertEquals($response->getData()->data->weekdays, 7);
+        $this->assertEquals($response->getData()->data->compete_weeks, 1);
+
+        $response = $this->get('http://127.0.0.1:8000/api/v1/datetimediff/2020-07-122/2020-07-13/');
+        $response->assertStatus(200);
+        $this->assertEquals($response->getData()->code, 40012);
+        $this->assertEquals($response->getData()->state, 'error');
+        $this->assertEquals($response->getData()->message, 'The start datetime format is not ISO 8601 format.');
+        
+        $response = $this->get('http://127.0.0.1:8000/api/v1/datetimediff/2020-07-22/2020-07-113/');
+        $response->assertStatus(200);
+        $this->assertEquals($response->getData()->code, 40012);
+        $this->assertEquals($response->getData()->state, 'error');
+        $this->assertEquals($response->getData()->message, 'The end datetime format is not ISO 8601 format.');
+        
+        $response = $this->get('http://127.0.0.1:8000/api/v1/datetimediff/2020-07-22/2020-07-13/second');
+        $response->assertStatus(200);
+        $this->assertEquals($response->getData()->code, 40012);
+        $this->assertEquals($response->getData()->state, 'error');
+        $this->assertEquals($response->getData()->message, 'The filter format is wrong.');
+
+        $response = $this->get('http://127.0.0.1:8000/api/v1/datetimediff/2020-07-22/2020-07-13/seconds');
+        $response->assertStatus(200);
+        $this->assertEquals($response->getData()->code, 200);
+        $this->assertEquals($response->getData()->state, 'success');
+        $this->assertEquals($response->getData()->message, 'Date time diff query succeed.');
+        $this->assertEquals($response->getData()->data->days_seconds, 777600);
+        $this->assertEquals($response->getData()->data->weekdays_seconds, 604800);
+        $this->assertEquals($response->getData()->data->compete_weeks_seconds, 604800);
     }
 }
